72b3cdcdf827c25cb35d689b810a7f2dacea0a0b
Ziv <zivlazarov@gmail.com>
2022-07-07 19:49:28 +0300

    added unreachable areas where player stops when interacting

---
+++ MyRPGGame/LandscapeEntity.cpp
@@ -0,0 +1,8 @@
+//
+//  LandscapeEntity.cpp
+//  MyRPGGame
+//
+//  Created by Ziv Lazarov on 07/07/2022.
+//
+
+#include "LandscapeEntity.hpp"
---
+++ MyRPGGame/LandscapeEntity.hpp
@@ -0,0 +1,13 @@
+//
+//  LandscapeEntity.hpp
+//  MyRPGGame
+//
+//  Created by Ziv Lazarov on 07/07/2022.
+//
+
+#ifndef LandscapeEntity_hpp
+#define LandscapeEntity_hpp
+
+#include <stdio.h>
+
+#endif /* LandscapeEntity_hpp */
