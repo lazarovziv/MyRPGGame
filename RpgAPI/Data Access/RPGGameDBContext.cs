183fbf1a2144b195e43c260a0a34eeb7467fed7a
Itay <lazarovitay@gmail.com>
2022-07-09 12:47:53 +0300

    Added the override OnModelCreating function in EF so it can Read the Enums (Player Type and Enemy Type) as ints.

--- RpgAPI/Data Access/RPGGameDBContext.cs
+++ RpgAPI/Data Access/RPGGameDBContext.cs
@@ -5,14 +5,27 @@ namespace RpgAPI.Data_Access
 {
     public class RPGGameDBContext : DbContext
     {
+        public DbSet<GameEntity> GameEntities => Set<GameEntity>();
+        public DbSet<GameMap> GameMaps => Set<GameMap>();
+        public DbSet<Player> Players => Set<Player>();
+        public DbSet<NPCEnemy> Enemies => Set<NPCEnemy>();
+
         public RPGGameDBContext(DbContextOptions<RPGGameDBContext> options) : base(options)
         {
 
         }
 
-        public DbSet<GameEntity> GameEntities => Set<GameEntity>();
-        public DbSet<GameMap> GameMaps => Set<GameMap>();
-        public DbSet<Player> Players => Set<Player>();
-        public DbSet<NPCEnemy> Enemies => Set<NPCEnemy>();
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<GameEntity>().ToTable("GameEntities");
+            builder.Entity<GameMap>().ToTable("GameMaps");
+            builder.Entity<Player>().ToTable("Players");
+            builder.Entity<NPCEnemy>().ToTable("Enemies");
+
+            builder.Entity<Player>().Property(p => p.PlayerType).HasConversion<int>();
+            builder.Entity<NPCEnemy>().Property(e => e.EnemyType).HasConversion<int>();
+
+            base.OnModelCreating(builder);
+        }
     }
 }
