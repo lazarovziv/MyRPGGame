28e3c095bb58d56cb355b3cd745a72ab6436fae4
Itay Lazarov <lazarovitay@gmail.com>
2022-07-25 19:07:25 +0300

    Edited the namespace syntax

--- RpgAPI/Models/Player.cs
+++ RpgAPI/Models/Player.cs
@@ -1,24 +1,23 @@
 ﻿using RpgAPI.Enums;
 using System.ComponentModel.DataAnnotations;
 
-namespace RpgAPI.Models
-{
-    public class Player
-    {
-        [Key]
-        public int PlayerId { get; set; }
-        [Required]
-        public int StrengthPoints { get; set; }
-        [Required]
-        public int IntelligencePoints { get; set; }
-        [Required]
-        public int CriticalPoints { get; set; }
-        [Required]
-        public int ExpPoints { get; set; }
-        [Required]
-        public PlayerType PlayerType { get; set; }
+namespace RpgAPI.Models;
 
-        //Foreign Key to Game Entity
-        public ICollection<GameEntity>? GameEntity { get; set; }
-    }
+public class Player
+{
+    [Key]
+    public int PlayerId { get; set; }
+    [Required]
+    public int StrengthPoints { get; set; }
+    [Required]
+    public int IntelligencePoints { get; set; }
+    [Required]
+    public int CriticalPoints { get; set; }
+    [Required]
+    public int ExpPoints { get; set; }
+    [Required]
+    public PlayerType PlayerType { get; set; }
+
+    //Foreign Key to Game Entity
+    public ICollection<GameEntity>? GameEntity { get; set; }
 }
