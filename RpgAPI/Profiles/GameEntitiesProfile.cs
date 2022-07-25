e59d431e4033f9ea2d25247cad2ec0ffc69ae5b6
Itay Lazarov <lazarovitay@gmail.com>
2022-07-25 19:08:06 +0300

    Commented the Create DTO functions of the models

--- RpgAPI/Profiles/GameEntitiesProfile.cs
+++ RpgAPI/Profiles/GameEntitiesProfile.cs
@@ -4,27 +4,26 @@ using RpgAPI.Dtos.ReadDtos;
 using RpgAPI.Dtos.UpdateDtos;
 using RpgAPI.Models;
 
-namespace RpgAPI.Profiles
+namespace RpgAPI.Profiles;
+
+public class GameEntitiesProfile : Profile
 {
-    public class GameEntitiesProfile : Profile
+    public GameEntitiesProfile()
     {
-        public GameEntitiesProfile()
-        {
-            //Source -> Target
-            CreateMap<GameEntity, GameEntityReadDto>();
-            CreateMap<Player, PlayerReadDto>();
-            CreateMap<GameMap, GameMapReadDto>();
-            CreateMap<NPCEnemy, NPCEnemyReadDto>();
+        //Source -> Target
+        CreateMap<GameEntity, GameEntityReadDto>();
+        CreateMap<Player, PlayerReadDto>();
+        CreateMap<GameMap, GameMapReadDto>();
+        CreateMap<NPCEnemy, NPCEnemyReadDto>();
 
-            CreateMap<GameEntityCreateDto, GameEntity>();
-            CreateMap<PlayerCreateDto, Player>();
-            CreateMap<GameEntityCreateDto, GameMap>();
-            CreateMap<NPCEnemyCreateDto, NPCEnemy>();
+        CreateMap<GameEntityCreateDto, GameEntity>();
+        CreateMap<PlayerCreateDto, Player>();
+        CreateMap<GameMapCreateDto, GameMap>();
+        CreateMap<NPCEnemyCreateDto, NPCEnemy>();
 
-            CreateMap<GameEntityUpdateDto, GameEntity>();
-            CreateMap<PlayerUpdateDto, Player>();
-            CreateMap<GameMapUpdateDto, GameMap>();
-            CreateMap<NPCEnemyUpdateDto, NPCEnemy>();
-        }
+        //CreateMap<GameEntityUpdateDto, GameEntity>();
+        //CreateMap<PlayerUpdateDto, Player>();
+        //CreateMap<GameMapUpdateDto, GameMap>();
+        //CreateMap<NPCEnemyUpdateDto, NPCEnemy>();
     }
 }
