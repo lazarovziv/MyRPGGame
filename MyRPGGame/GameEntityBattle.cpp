26af1299c8584d783cd7975868f711431cabcbee
Ziv <zivlazarov@gmail.com>
2022-11-09 15:53:37 +0200

    added a battle system class for handling various battle scenarios. implmented the attack function for now

---
+++ MyRPGGame/GameEntityBattle.cpp
@@ -0,0 +1,8 @@
+//
+//  GameEntityBattle.cpp
+//  MyRPGGame
+//
+//  Created by Ziv Lazarov on 07/11/2022.
+//
+
+#include "GameEntityBattle.hpp"
---
+++ MyRPGGame/GameEntityBattle.hpp
@@ -0,0 +1,13 @@
+//
+//  GameEntityBattle.hpp
+//  MyRPGGame
+//
+//  Created by Ziv Lazarov on 07/11/2022.
+//
+
+#ifndef GameEntityBattle_hpp
+#define GameEntityBattle_hpp
+
+#include <stdio.h>
+
+#endif /* GameEntityBattle_hpp */
