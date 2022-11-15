5bb29c31b551458ff426c6c4e7ba6fe3e2b7e7c5
Ziv <zivlazarov@gmail.com>
2022-11-15 14:14:54 +0200

    fixed install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Press y when possible to install necessary libraries"
-sudo apt-get install build-essentials libsfml-dev
+sudo apt-get install build-essential libsfml-dev
 chmod +x install.sh
