72079f1620b863bcaaaecf53174f844b4b3be38b
Ziv <zivlazarov@gmail.com>
2023-04-02 11:33:28 +0300

    Added constants. added NUM_ROWS = NUM_COLS = 3 for world map usage

--- MyRPGGame/include/Constants.h
+++ MyRPGGame/include/Constants.h
@@ -7,6 +7,8 @@ public:
     static const int SCREEN_HEIGHT = 600;
     static const int TILE_SIZE = 64;
     static const int FPS = 60;
+    static const int NUM_ROWS = 3;
+    static const int NUM_COLS = 3;
 };
 
 #endif /* Constants_h */
