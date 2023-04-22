9ec31a6dc1ec959946ced413f4093fda784ed13c
Ziv <zivlazarov@gmail.com>
2023-04-23 02:53:37 +0300

    Changed window dimensions. changed width to 640 (was 600) for being divisable by 16.

--- MyRPGGame/include/Constants.h
+++ MyRPGGame/include/Constants.h
@@ -4,8 +4,9 @@
 class Constants {
 public:
     static const int SCREEN_WIDTH = 800;
-    static const int SCREEN_HEIGHT = 600;
+    static const int SCREEN_HEIGHT = 640;
     static const int TILE_SIZE = 64;
+    static const int BASE_ENTITY_SPEED = 16;
     static const int FPS = 60;
     static const int NUM_ROWS = 3;
     static const int NUM_COLS = 3;
