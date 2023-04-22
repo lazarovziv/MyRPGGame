682a081abe69df8bb45d7cb4e6cf9357ae3c113c
Ziv <zivlazarov@gmail.com>
2023-04-23 02:14:24 +0300

    Added generics to graph. Will use Point from now on. moved all function declarations to hpp file.

--- MyRPGGame/include/Graph.hpp
+++ MyRPGGame/include/Graph.hpp
@@ -8,30 +8,109 @@
 #include <queue>
 #include "GameEntity.hpp"
 
+template<typename T1> struct greaterPair {
+    constexpr bool operator()(const std::pair<T1, int> &first, const std::pair<T1, int> &second) const {
+        return first.second > second.second;
+    }
+};
+
+template <class V>
 class Graph {
 private:
     std::map<GameEntity *, std::vector<std::pair<GameEntity *, int>> *> adjVertices;
     std::vector<GameEntity *> vertices;
+
+    std::map<V, std::vector<std::pair<V, int>> *> genericGraph;
+    std::vector<V> genericVertices;
 public:
-    Graph();
+    Graph() = default;
     //add the node to the graph
-    void addVertex(GameEntity *vertex);
-    void removeVertex(GameEntity *vertex);
+//    void addVertex(GameEntity *vertex);
+    void addVertex(V vertex) {
+        genericGraph[vertex] = new std::vector<std::pair<V, int>>();
+        genericVertices.push_back(vertex);
+    }
+//    void removeVertex(GameEntity *vertex);
+    void removeVertex(V vertex) {
+//    auto iterator = adjVertices.find(vertex);
+        genericGraph.erase(vertex);
+        genericVertices.erase(std::find(genericVertices.begin(), genericVertices.end(), vertex));
+    }
     //Add edge between two nodes
-    void addEdge(GameEntity * vertex1, GameEntity * vertex2, int edge);
+//    void addEdge(GameEntity *vertex1, GameEntity *vertex2, int edge);
+    void addEdge(V vertex1, V vertex2, int edge) {
+        genericGraph[vertex1]->push_back(make_pair(vertex2, edge));
+    }
 
-    std::map<GameEntity *, GameEntity *> dijkstra(GameEntity * source);
+//    std::map<GameEntity *, GameEntity *> dijkstra(GameEntity * source);
+    std::map<V, V> dijkstra(V source) {
+        std::map<V, int> distances;
+        std::map<V, V> previous;
+        std::map<V, bool> visited;
+        std::priority_queue<std::pair<V, int>, std::vector<std::pair<V, int>>, greaterPair<V>> queue;
+        V u, v;
+        int weight = 0;
+        int countVisited = 0;
+        int verticesSize = getSize();
+        int alt = INT32_MAX;
 
-    void clear();
+        distances[source] = 0;
+        for (auto &item : genericGraph) {
+            if (item.first != source) {
+                distances[item.first] = INT32_MAX;
+                visited[item.first] = false;
+            }
+            queue.emplace(item.first, distances[item.first]);
+        }
+        visited[source] = true;
+        countVisited++;
+
+        while (countVisited != verticesSize) {
+            u = queue.top().first;
+            queue.pop();
+
+            for (int i = 0; i < genericGraph[u]->size(); i++) {
+                v = genericGraph[u]->at(i).first;
+                weight = genericGraph[u]->at(i).second;
+                alt = distances[u] + weight;
+
+                if (alt < distances[v]) {
+                    distances[v] = alt;
+                    previous[v] = u;
+                    queue.emplace(v, alt);
+                }
+            }
+            countVisited++;
+        }
+
+        return previous;
+    }
+
+    void clear() {
+        adjVertices.clear();
+        vertices.clear();
+        genericGraph.clear();
+        genericVertices.clear();
+    }
 
     //returns all the vertices connected to it
-    std::vector<std::pair<GameEntity *, int>> *getNeighbors(GameEntity * vertex);
-    std::vector<GameEntity *> getVertices();
+//    std::vector<std::pair<GameEntity *, int>> *getNeighbors(GameEntity *vertex);
+    std::vector<std::pair<V, int>> *getNeighbors(V vertex) {
+        return genericGraph[vertex];
+    }
+//    std::vector<GameEntity *> getVertices();
+    std::vector<V> getVertices() {
+        return vertices;
+    }
 
     //returns the number of vertices in the graph
-    int getSize();
+//    int getSize();
+    int getSize() {
+        return genericGraph.size();
+    }
 
-    void printPath(const std::map<GameEntity *, GameEntity *>& path);
+//    void printPath(const std::map<GameEntity *, GameEntity *>& path);
+//    void printPath(const std::map<V, V>& path);
 };
 
 #endif //Graph_HPP
--- MyRPGGame/src/Graph.cpp
+++ MyRPGGame/src/Graph.cpp
@@ -1,90 +1,179 @@
 #include "../include/Graph.hpp"
 
-template<typename T1> struct greaterPair {
-    constexpr bool operator()(const std::pair<T1, int> &first, const std::pair<T1, int> &second) const {
-        return first.second > second.second;
-    }
-};
+//template<typename T1> struct greaterPair {
+//    constexpr bool operator()(const std::pair<T1, int> &first, const std::pair<T1, int> &second) const {
+//        return first.second > second.second;
+//    }
+//};
 
-Graph::Graph() = default;
+//template <class V>
+//Graph<V>::Graph() = default;
 
-void Graph::addVertex(GameEntity *vertex) {
-    adjVertices[vertex] = new std::vector<std::pair<GameEntity *, int>>();
-    vertices.push_back(vertex);
-}
+//template <class V>
+//void Graph<V>::addVertex(GameEntity *vertex) {
+//    adjVertices[vertex] = new std::vector<std::pair<GameEntity *, int>>();
+//    vertices.push_back(vertex);
+//}
 
-void Graph::removeVertex(GameEntity *vertex) {
-//    auto iterator = adjVertices.find(vertex);
-    adjVertices.erase(vertex);
-    vertices.erase(std::find(vertices.begin(), vertices.end(), vertex));
-}
+//template <class V>
+//void Graph<V>::addVertex(V vertex) {
+//    genericGraph[vertex] = new std::vector<std::pair<V, int>>();
+//    genericVertices.push_back(vertex);
+//}
 
-void Graph::addEdge(GameEntity * vertex1, GameEntity * vertex2, int edge) {
-    adjVertices[vertex1]->push_back(make_pair(vertex2, edge));
-}
+//template <class V>
+//void Graph<V>::removeVertex(GameEntity *vertex) {
+////    auto iterator = adjVertices.find(vertex);
+//    adjVertices.erase(vertex);
+//    vertices.erase(std::find(vertices.begin(), vertices.end(), vertex));
+//}
 
-std::map<GameEntity *, GameEntity *> Graph::dijkstra(GameEntity * source) {
-    std::map<GameEntity *, int> distances;
-    std::map<GameEntity *, GameEntity *> previous;
-    std::map<GameEntity *, bool> visited;
-    std::priority_queue<std::pair<GameEntity *, int>, std::vector<std::pair<GameEntity *, int>>, greaterPair<GameEntity *>> queue;
-    GameEntity *u, *v;
-    int weight = 0;
-    int countVisited = 0;
-    int verticesSize = getSize();
-    int alt = INT32_MAX;
+//template <class V>
+//void Graph<V>::removeVertex(V vertex) {
+////    auto iterator = adjVertices.find(vertex);
+//    genericGraph.erase(vertex);
+//    genericVertices.erase(std::find(genericVertices.begin(), genericVertices.end(), vertex));
+//}
 
-    distances[source] = 0;
-    for (auto &item : adjVertices) {
-        if (item.first != source) {
-            distances[item.first] = INT32_MAX;
-            visited[item.first] = false;
-        }
-        queue.emplace(item.first, distances[item.first]);
-    }
-    visited[source] = true;
-    countVisited++;
+//template <class V>
+//void Graph<V>::addEdge(GameEntity * vertex1, GameEntity * vertex2, int edge) {
+//    adjVertices[vertex1]->push_back(make_pair(vertex2, edge));
+//}
 
-    while (countVisited != verticesSize) {
-        u = queue.top().first;
-        queue.pop();
+//template <class V>
+//void Graph<V>::addEdge(V vertex1, V vertex2, int edge) {
+//    genericGraph[vertex1]->push_back(make_pair(vertex2, edge));
+//}
 
-        for (int i = 0; i < adjVertices[u]->size(); i++) {
-            v = adjVertices[u]->at(i).first;
-            weight = adjVertices[u]->at(i).second;
-            alt = distances[u] + weight;
+//template <class V>
+//std::map<GameEntity *, GameEntity *> Graph<V>::dijkstra(GameEntity * source) {
+//    std::map<GameEntity *, int> distances;
+//    std::map<GameEntity *, GameEntity *> previous;
+//    std::map<GameEntity *, bool> visited;
+//    std::priority_queue<std::pair<GameEntity *, int>, std::vector<std::pair<GameEntity *, int>>, greaterPair<GameEntity *>> queue;
+//    GameEntity *u, *v;
+//    int weight = 0;
+//    int countVisited = 0;
+//    int verticesSize = getSize();
+//    int alt = INT32_MAX;
+//
+//    distances[source] = 0;
+//    for (auto &item : adjVertices) {
+//        if (item.first != source) {
+//            distances[item.first] = INT32_MAX;
+//            visited[item.first] = false;
+//        }
+//        queue.emplace(item.first, distances[item.first]);
+//    }
+//    visited[source] = true;
+//    countVisited++;
+//
+//    while (countVisited != verticesSize) {
+//        u = queue.top().first;
+//        queue.pop();
+//
+//        for (int i = 0; i < adjVertices[u]->size(); i++) {
+//            v = adjVertices[u]->at(i).first;
+//            weight = adjVertices[u]->at(i).second;
+//            alt = distances[u] + weight;
+//
+//            if (alt < distances[v]) {
+//                distances[v] = alt;
+//                previous[v] = u;
+//                queue.emplace(v, alt);
+//            }
+//        }
+//        countVisited++;
+//    }
+//
+//    return previous;
+//}
 
-            if (alt < distances[v]) {
-                distances[v] = alt;
-                previous[v] = u;
-                queue.emplace(v, alt);
-            }
-        }
-        countVisited++;
-    }
+//template <class V>
+//std::map<V, V> Graph<V>::dijkstra(V source) {
+//    std::map<V, int> distances;
+//    std::map<V, V> previous;
+//    std::map<V, bool> visited;
+//    std::priority_queue<std::pair<V, int>, std::vector<std::pair<V, int>>, greaterPair<V>> queue;
+//    V *u, *v;
+//    int weight = 0;
+//    int countVisited = 0;
+//    int verticesSize = getSize();
+//    int alt = INT32_MAX;
+//
+//    distances[source] = 0;
+//    for (auto &item : genericGraph) {
+//        if (item.first != source) {
+//            distances[item.first] = INT32_MAX;
+//            visited[item.first] = false;
+//        }
+//        queue.emplace(item.first, distances[item.first]);
+//    }
+//    visited[source] = true;
+//    countVisited++;
+//
+//    while (countVisited != verticesSize) {
+//        u = queue.top().first;
+//        queue.pop();
+//
+//        for (int i = 0; i < genericGraph[u]->size(); i++) {
+//            v = genericGraph[u]->at(i).first;
+//            weight = genericGraph[u]->at(i).second;
+//            alt = distances[u] + weight;
+//
+//            if (alt < distances[v]) {
+//                distances[v] = alt;
+//                previous[v] = u;
+//                queue.emplace(v, alt);
+//            }
+//        }
+//        countVisited++;
+//    }
+//
+//    return previous;
+//}
 
-    return previous;
-}
+//template <class V>
+//void Graph<V>::clear() {
+//    adjVertices.clear();
+//    vertices.clear();
+//    genericGraph.clear();
+//    genericVertices.clear();
+//}
 
-void Graph::clear() {
-    adjVertices.clear();
-    vertices.clear();
-}
+//template <class V>
+//std::vector<std::pair<GameEntity *, int>> *Graph<V>::getNeighbors(GameEntity *vertex) {
+//    return adjVertices[vertex];
+//}
 
-std::vector<std::pair<GameEntity *, int>> *Graph::getNeighbors(GameEntity *vertex) {
-    return adjVertices[vertex];
-}
+//template <class V>
+//std::vector<std::pair<V, int>> *Graph<V>::getNeighbors(V vertex) {
+//    return genericGraph[vertex];
+//}
 
-std::vector<GameEntity *> Graph::getVertices() {
-    return vertices;
-}
+//template <class V>
+//std::vector<V> Graph<V>::getVertices() {
+//    return vertices;
+//}
 
-int Graph::getSize() {
-    return adjVertices.size();
-}
+//template <class V>
+//std::vector<V> Graph<V>::getGenericVertices() {
+//    return genericVertices;
+//}
 
-void Graph::printPath(const std::map<GameEntity *, GameEntity *>& path) {
-    for (auto &pair : path) {
-        cout << "(" << pair.second->getPosition().x << ", " << pair.second->getPosition().y << ") -> (" << pair.first->getPosition().x << ", " << pair.first->getPosition().y << ")" << endl;
-    }
-}
+//template <class V>
+//int Graph<V>::getSize() {
+//    return genericGraph.size();
+//}
+
+//template <class V>
+//int Graph<V>::getGenericSize() {
+//    return genericGraph.size();
+//}
+
+//template <class V>
+//void Graph<V>::printPath(const std::map<GameEntity *, GameEntity *>& path) {
+//    for (auto &pair : path) {
+//        cout << "(" << pair.second->getPosition().x << ", " << pair.second->getPosition().y << ") -> (" << pair.first->getPosition().x << ", " << pair.first->getPosition().y << ")" << endl;
+//    }
+//}
