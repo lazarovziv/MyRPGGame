8345c08012803dfe3450b2003b1764a2eb3db9eb
Ziv <zivlazarov@gmail.com>
2023-05-24 14:40:06 +0300

    Added FULL_SCREEN_* constants for using in camera view. Made FULL_SCREEN_* variables as the entire map size where SCREEN_* variables are the window dimensions.

--- MyRPGGame/include/Constants.h
+++ MyRPGGame/include/Constants.h
@@ -5,14 +5,17 @@ class Constants {
 public:
     static const int SCREEN_WIDTH = 800;
     static const int SCREEN_HEIGHT = 640;
+    static const int FULL_SCREEN_WIDTH = 4 * SCREEN_WIDTH;
+    static const int FULL_SCREEN_HEIGHT = 3 * SCREEN_HEIGHT;
     static const int TILE_SIZE = 64;
     static const int BASE_ENTITY_SPEED = 16;
     static const int FPS = 60;
     static const int NUM_ROWS = 3;
     static const int NUM_COLS = 3;
 
+    // enum for distinguishing moves - CHANGE_* means needs to change to relevant map
     enum MoveSuccessValues {
-        FAILURE, SUCCESS, CHANGE_UP, CHANGE_DOWN, CHANGE_RIGHT, CHANGE_LEFT
+        NOT_MOVED, FAILURE, SUCCESS, CHANGE_UP, CHANGE_DOWN, CHANGE_RIGHT, CHANGE_LEFT
     };
 };
 
