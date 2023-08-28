904432eebc5f216e740167787f888893f4b19a50
Ziv <zivlazarov@gmail.com>
2023-08-28 03:25:09 +0300

    Started implementing weapons animations system. added sprite for every weapon and relevant fields to handle collisions with enemies.

--- MyRPGGame/include/Weapon.hpp
+++ MyRPGGame/include/Weapon.hpp
@@ -3,14 +3,15 @@
 #ifndef Weapon_hpp
 #define Weapon_hpp
 
-#include <stdio.h>
+#include <iostream>
+#include <map>
 #include <string.h>
 #include <SFML/Graphics.hpp>
+#include "Constants.h"
+#include "Circle.hpp"
 
 using namespace sf;
 using namespace std;
-
-enum class WeaponType { SWORD, DAGGER, STAFF, AXE, MACE, BARE_HANDED };
 
 class Weapon {
 private:
@@ -18,15 +19,34 @@ private:
     int attackPoints;
     float hitRadius;
     int numHitsPerSecond;
-    Sprite sprite;
+
+    MoveDirection transitionDirection; // copying the entity's direction (simply for choosing the appropriate rows in the png file)
+    map<MoveDirection, int> transitionDirectionsSpritesMap; // for defining movement and animation for the pngs
+    Vector2i position; // for collision detection
+    Texture texture;
+    Sprite *sprite;
+    IntRect spriteRect; // for setting the relevant image from the png
+    int transition = Constants::WEAPON_TRANSITION_MIN;
+
+    Circle *weaponCircle = nullptr;
     
 public:
-    Weapon(WeaponType type);
+    Weapon(Point *center, WeaponType type);
     ~Weapon() = default;
     
     int getAttackPoints();
     int getNumHitsPerSecond();
     float getHitRadius();
+
+    MoveDirection getTransitionDirection();
+    map<MoveDirection, int> getTransitionDirectionsSpritesMap();
+    Vector2i getPosition();
+    Sprite *getSprite();
+    Circle *getWeaponCircle();
+    int getTransition();
+    void incrementTransition();
+
+    void setTransitionDirection(MoveDirection direction);
     
     void increaseAttackPoints(int amount);
     void increaseHitRadius(float amount);
@@ -34,7 +54,11 @@ public:
     void decreaseAttackPoints(int amount);
     void decreaseHitRadius(float amount);
     void decreaseNumHitsPerSecond(int amount);
+    // call this function inside the battleMovementHandler
+    void setIntRectPosition(int left, int top, int width, int height);
 //    void attack();
+
+    void update(Point ***point);
 };
 
 #endif /* Weapon_hpp */
--- MyRPGGame/src/Weapon.cpp
+++ MyRPGGame/src/Weapon.cpp
@@ -1,12 +1,31 @@
 #include "../include/Weapon.hpp"
 
-Weapon::Weapon(WeaponType type) {
+Weapon::Weapon(Point *center, WeaponType type) {
+    // TODO: handle shield differently with the directions map
+    transitionDirectionsSpritesMap[MoveDirection::UP] = 1;
+    transitionDirectionsSpritesMap[MoveDirection::LEFT] = 2;
+    transitionDirectionsSpritesMap[MoveDirection::DOWN] = 3;
+    transitionDirectionsSpritesMap[MoveDirection::RIGHT] = 4;
+
+    weaponCircle = new Circle(center, (float) Constants::TILE_SIZE/4);
+
     switch (type) {
         case WeaponType::SWORD:
             name = "Sword";
             attackPoints = 10;
             hitRadius = 5 * 6.5f;
             numHitsPerSecond = 3;
+            // init graphics
+            sprite = new Sprite();
+            if (!texture.loadFromFile("../graphics/images/weapons/oversize/right_hand/male/longsword_male.png")) {
+                std::cout << "Texture NOT loaded properly!" << endl;
+                texture.setSmooth(true);
+            } else std::cout << "Weapon loaded properly." << endl;
+            sprite->setTexture(texture);
+            sprite->setTextureRect(sf::IntRect(transitionDirectionsSpritesMap[transitionDirection]*Constants::TILE_SIZE, 0, Constants::TILE_SIZE, Constants::TILE_SIZE));
+            sprite->setOrigin(Constants::TILE_SIZE/2, Constants::TILE_SIZE/2);
+            sprite->setPosition(position.x, position.y);
+
             break;
         case WeaponType::DAGGER:
             name = "Dagger";
@@ -39,6 +58,9 @@ Weapon::Weapon(WeaponType type) {
             numHitsPerSecond = 3;
             break;
     }
+
+    position.x = weaponCircle->getCenter()->getX();
+    position.y = weaponCircle->getCenter()->getY();
 }
 
 void Weapon::increaseAttackPoints(int amount) {
@@ -81,4 +103,52 @@ float Weapon::getHitRadius() {
 
 int Weapon::getNumHitsPerSecond() {
     return numHitsPerSecond;
+}
+
+void Weapon::setIntRectPosition(int left, int top, int width, int height) {
+    spriteRect.left = left;
+    spriteRect.top = top;
+    spriteRect.width = width;
+    spriteRect.height = height;
+    sprite->setTextureRect(spriteRect);
+}
+
+MoveDirection Weapon::getTransitionDirection() {
+    return transitionDirection;
+}
+
+map<MoveDirection, int> Weapon::getTransitionDirectionsSpritesMap() {
+    return transitionDirectionsSpritesMap;
+}
+
+Vector2i Weapon::getPosition() {
+    return position;
+}
+
+Sprite* Weapon::getSprite() {
+    return sprite;
+}
+
+Circle* Weapon::getWeaponCircle() {
+    return weaponCircle;
+}
+
+int Weapon::getTransition() {
+    return transition;
+}
+
+void Weapon::incrementTransition() {
+    // defined by the pngs order
+    // TODO: add an extra field for identifying another weapons which uses different logic
+    if (transition < Constants::WEAPON_TRANSITION_MAX) transition++;
+    else transition = Constants::WEAPON_TRANSITION_MIN;
+}
+
+void Weapon::setTransitionDirection(MoveDirection direction) {
+    transitionDirection = direction;
+}
+
+void Weapon::update(Point ***points) {
+    sprite->setPosition(position.x, position.y);
+    weaponCircle->setCenter(points[position.y][position.x]);
 }
