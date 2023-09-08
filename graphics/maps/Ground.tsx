f5ac04f650d6882c97f9b3abdbecc1e29e2ea912
Ziv <zivlazarov@gmail.com>
2023-09-08 22:33:11 +0300

    Added images for creating the spritesheets and the Credits.txt. created hierarchy for the images generations.

---
+++ graphics/maps/Ground.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Ground" tilewidth="64" tileheight="64" tilecount="132" columns="12">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="../../../../../Downloads/resize_tilemap_packed.png" width="768" height="704"/>
+</tileset>
