94cb9bcd910190662ae36afa47d9d76f04b4dd9b
Ziv <zivlazarov@gmail.com>
2023-09-22 19:39:38 +0300

    fixed run_linux.sh script. fixed if statements

--- run_linux.sh
+++ run_linux.sh
@@ -1,31 +1,34 @@
 #!/bin/bash
 
-if [ ! command -v <cmake> &> /dev/null ] || [ ! command -v <ninja> &> /dev/null ]; then
+# ubuntu based :)
+# TODO: add other distros
+# if cmake and ninja aren't installed on the system
+if ! [ which cmake | grep "not found" -c ] || ! [ which ninja | grep "not found" -c ]; then
   echo "cmake or ninja aren't installed. Install? [y/n]"
-  read
+  read REPLY
   if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
     echo "Installing..."
     sudo apt-get cmake ninja-build -y
   fi
 fi
 
-if [ ! dpkg -s libsfml-dev &> /dev/null ]; then
+if ! [ dpkg -s libsfml-dev &> /dev/null ]; then
   echo "SFML library isn't installed. Install? [y/n]"
-  read
+  read reply
   if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
     echo "Installing..."
     sudo apt-get libsfml-dev -y
   fi
 fi
 
-DIR=build
-if [ -d "$DIR" ]
-then
-  echo "Directory already exists. Deleting..."
-  rm -rf build
-fi
+# DIR=build
+# if [ -d "$DIR" ]
+# then
+#   echo "Directory already exists. Deleting..."
+#   rm -rf build
+# fi
 
-mkdir build
+# mkdir build
 cmake -B build/ -G Ninja
 cd build
 ninja
