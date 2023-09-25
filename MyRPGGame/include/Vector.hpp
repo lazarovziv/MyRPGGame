2de3838373b6b2f168be7b068cc0c3954f60251f
Ziv <zivlazarov@gmail.com>
2023-09-25 14:52:57 +0300

    Added Vector and physics namespace.

---
+++ MyRPGGame/include/Vector.hpp
@@ -0,0 +1,45 @@
+#ifndef MYRPGGAME_VECTOR_HPP
+#define MYRPGGAME_VECTOR_HPP
+
+#include "Constants.h"
+#include <stdexcept>
+
+namespace physics {
+
+    class Vector {
+    private:
+        real x;
+        real y;
+        real z;
+    public:
+        Vector(real x, real y, real z = 0) : x(x), y(y), z(z) {};
+        ~Vector() = default;
+
+        real norma();
+        Vector normalize();
+        real dot(const Vector &other);
+
+        void operator +=(const Vector &other);
+        void operator *=(const Vector &other);
+        void operator -=(const Vector &other);
+        void operator /=(const Vector &other);
+        Vector operator +(const Vector &other) const;
+        Vector operator *(const Vector &other) const;
+        Vector operator -(const Vector &other) const;
+        Vector operator /(const Vector &other) const;
+
+        void operator +=(const real scalar);
+        void operator *=(const real scalar);
+        void operator -=(const real scalar);
+        void operator /=(const real scalar);
+        Vector operator +(const real scalar) const;
+        Vector operator *(const real scalar) const;
+        Vector operator -(const real scalar) const;
+        Vector operator /(const real scalar) const;
+
+        bool operator ==(const Vector &other) const;
+        bool operator !=(const Vector &other) const;
+    };
+}
+
+#endif //MYRPGGAME_VECTOR_HPP
---
+++ MyRPGGame/src/Vector.cpp
@@ -0,0 +1,119 @@
+#include "../include/Vector.hpp"
+
+namespace physics {
+
+    real Vector::norma() {
+        return x*x + y*y + z*z;
+    }
+
+    Vector Vector::normalize() {
+        return (*this) / norma();
+    }
+
+    real Vector::dot(const physics::Vector &other) {
+        return x * other.x + y * other.y + z * other.z;
+    }
+
+    void Vector::operator +=(const Vector &other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+    }
+
+    void Vector::operator *=(const Vector &other) {
+        x *= other.x;
+        y *= other.y;
+        z *= other.z;
+    }
+
+    void Vector::operator -=(const Vector &other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+    }
+
+    void Vector::operator /=(const Vector &other) {
+        if (other.x == 0 || other.y == 0 || other.z == 0) {
+            throw std::runtime_error("Division by zero!");
+        }
+        x /= other.x;
+        y /= other.y;
+        z /= other.z;
+    }
+
+    Vector Vector::operator +(const Vector &other) const {
+        Vector result(x, y, z);
+        result += other;
+        return result;
+    }
+
+    Vector Vector::operator *(const Vector &other) const {
+        Vector result(x, y, z);
+        result *= other;
+        return result;
+    }
+
+    Vector Vector::operator -(const Vector &other) const {
+        Vector result(x, y, z);
+        result -= other;
+        return result;
+    }
+
+    Vector Vector::operator /(const Vector &other) const {
+        Vector result(x, y, z);
+        result /= other;
+        return result;
+    }
+
+    void Vector::operator +=(const real scalar) {
+        x += scalar;
+        y += scalar;
+        z += scalar;
+    }
+
+    void Vector::operator *=(const real scalar) {
+        x *= scalar;
+        y *= scalar;
+        z *= scalar;
+    }
+
+    void Vector::operator -=(const real scalar) {
+        x -= scalar;
+        y -= scalar;
+        z -= scalar;
+    }
+
+    void Vector::operator /=(const real scalar) {
+        if (scalar == 0) {
+            throw std::runtime_error("Division by zero!");
+        }
+        x /= scalar;
+        y /= scalar;
+        z /= scalar;
+    }
+
+    Vector Vector::operator +(const real scalar) const {
+        Vector result(x, y, z);
+        result += scalar;
+        return result;
+    }
+
+    Vector Vector::operator *(const real scalar) const {
+        Vector result(x, y, z);
+        result *= scalar;
+        return result;
+    }
+
+    Vector Vector::operator -(const real scalar) const {
+        Vector result(x, y, z);
+        result -= scalar;
+        return result;
+    }
+
+    Vector Vector::operator /(const real scalar) const {
+        Vector result(x, y, z);
+        result /= scalar;
+        return result;
+    }
+
+}
