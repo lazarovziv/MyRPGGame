835e722a87ba7323aac34b7424e759d52eff3d93
Ziv <zivlazarov@gmail.com>
2023-10-01 21:12:43 +0300

    Improved the circle-circle collision detection. simplified the process by making it more generic for future circle-polygon collision detection.

--- MyRPGGame/src/Collision.cpp
+++ MyRPGGame/src/Collision.cpp
@@ -3,37 +3,29 @@
 namespace physics {
 
     bool physics::resolveCollisions(physics::RigidBody *first, physics::RigidBody *second, real dt) {
-        // referring to first and second as of type Circle
+        // getting vector between positions
         Vector separator = second->getPosition() - first->getPosition();
         Vector relativeDirection = separator.normalized();
+        // checking moving in the same direction
         Vector relativeVelocity = first->getVelocity() - second->getVelocity();
-        // if moving in the same direction
         if (relativeVelocity.dot(relativeDirection) > 0) return false;
-        real penetrationDistance = 0;
-        // divide into different types of bodies
+        // calculating the penetration
+        real penetrationDistance = Constants::REAL_MAX;
+        // dividing into all types of bodies
         if (first->getBodyType() == RigidBodyType::CIRCLE) {
             if (second->getBodyType() == RigidBodyType::CIRCLE) {
+                // distance of the overlapping area
                 penetrationDistance = ((Circle*)first)->getRadius() + ((Circle*)second)->getRadius() - separator.norma();
-            } else if (second->getBodyType() == RigidBodyType::BOX) {
-                Vector firstSeparatorDistance = Vector{abs(first->getPosition().x - second->getPosition().x),
-                                                       abs(first->getPosition().y - second->getPosition().y)};
-                if (firstSeparatorDistance.x > ((Box*)second)->getWidth()/2 + ((Circle*)first)->getRadius()) return false;
-                if (firstSeparatorDistance.y > ((Box*)second)->getHeight()/2 + ((Circle*)first)->getRadius()) return false;
-                penetrationDistance = firstSeparatorDistance.distance(second->getPosition());
-                if (penetrationDistance <= pow(((Circle*)first)->getRadius(), 2)) return false;
+            } else if (second->getBodyType() == RigidBodyType::POLYGON) {
+
             }
-        } else if (first->getBodyType() == RigidBodyType::BOX) {
+        } else if (first->getBodyType() == RigidBodyType::POLYGON) {
             if (second->getBodyType() == RigidBodyType::CIRCLE) {
-                Vector secondSeparatorDistance = Vector{abs(second->getPosition().x - first->getPosition().x),
-                                                       abs(second->getPosition().y - first->getPosition().y)};
-                if (secondSeparatorDistance.x > ((Box*)first)->getWidth()/2 + ((Circle*)second)->getRadius()) return false;
-                if (secondSeparatorDistance.y > ((Box*)first)->getHeight()/2 + ((Circle*)second)->getRadius()) return false;
-                penetrationDistance = secondSeparatorDistance.distance(first->getPosition());
-                if (penetrationDistance <= pow(((Circle*)second)->getRadius(), 2)) return false;
-            } else if (second->getBodyType() == RigidBodyType::BOX) {
-                // TODO: todo
+            } else if (second->getBodyType() == RigidBodyType::POLYGON) {
+
             }
         } // more body types
+
         // calculated relevant penetration distance
         if (penetrationDistance < 0) return false;
         // setting the separation distance for each body
@@ -41,6 +33,11 @@ namespace physics {
         real secondDistance = first->getMass()/(first->getMass() + second->getMass());
         (*first) += relativeDirection * dt * -penetrationDistance * firstDistance;
         (*second) += relativeDirection * dt * penetrationDistance * secondDistance;
+
         return true;
+    }
+
+    bool physics::isCirclePolygonColliding(physics::Circle *first, physics::Polygon *second) {
+        return false;
     }
 }
