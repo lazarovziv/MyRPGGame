7bd137237abb8bb5793e5e18d70c3b5a0dfd8420
Ziv <zivlazarov@gmail.com>
2024-02-10 10:09:17 +0200

    cmake: made build process more generic. now supporting windows compilations - requires VS 17 2022 installed. added windows script for easier triggering of compilations and modified macOS existing script.

--- run_macos.sh
+++ run_macos.sh
@@ -1,19 +1,12 @@
 #!/bin/zsh
 
-# remove mac os compiler comment
-sed -i '' -e 's/\#//g' CMakeLists.txt
+cmake -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release .
+cmake --build build/ --config Release
 
-DIR=build
-if [ -d "$DIR" ]
-then
-  echo "Directory already exists. Deleting..."
-  rm -rf build
+echo "Run? (y/n)"
+read ANSWER
+
+if [ "$ANSWER"="y" ]; then
+  cd build
+  ./main
 fi
-
-mkdir build
-cmake -B build/ -G Ninja
-cd build
-ninja
-./main.out
-
-echo "To rerun, enter build folder and run main.out executable."
