df61ee17772e49bcab8789a897cc8579f3fee3d5
Ziv Lazarov <zivlazarov@gmail.com>
2024-07-12 05:27:36 +0300

    docker: modified linux to run inside docker moved main executable generation to build/bin/main instead of build/MyRPGGame/core/main

--- linux-compile.sh
+++ linux-compile.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-# ubuntu based :)
-# TODO: add other distros
-# if cmake and ninja aren't installed on the system
-if ! [ which cmake | grep "not found" -c ] || ! [ which ninja | grep "not found" -c ]; then
-  echo "cmake or ninja aren't installed. Install? [y/n]"
-  read REPLY
-  if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
-    echo "Installing..."
-    sudo apt-get cmake ninja-build -y
-  fi
+if [ "$1" == "start" ]; then
+  echo "Compiling and Starting..."
+#  docker-compose -f dockerfiles/linux-start-docker-compose.yml up
+  docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -e DISPLAY=$DISPLAY --net host --name rpg rpg \
+    bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/ && cd /workspace/build/bin && ./main"
+else
+  echo "Compiling..."
+#  docker-compose -f dockerfiles/linux-compile-docker-compose.yml up
+  docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -e DISPLAY=$DISPLAY --net host --name rpg rpg \
+    bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/"
 fi
-
-if ! [ dpkg -s libsfml-dev &> /dev/null ]; then
-  echo "SFML library isn't installed. Install? [y/n]"
-  read reply
-  if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
-    echo "Installing..."
-    sudo apt-get libsfml-dev -y
-  fi
-fi
-
-# DIR=build
-# if [ -d "$DIR" ]
-# then
-#   echo "Directory already exists. Deleting..."
-#   rm -rf build
-# fi
-
-# mkdir build
-cmake -B build/ -G Ninja
-cd build
-ninja
-./main.out
-
-echo "To rerun, enter build folder and run main.out executable."
