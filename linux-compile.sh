2e7bcb4242ccb689ee5b871b67800cf7c58e1b76
Ziv Lazarov <zivlazarov@gmail.com>
2024-07-25 03:31:54 +0300

    automation & docker: added device arguments to let SFML use relevant devices

--- linux-compile.sh
+++ linux-compile.sh
@@ -3,7 +3,8 @@
 if [ "$1" == "run" ]; then
   echo "Compiling and running..."
 #  docker-compose -f dockerfiles/linux-start-docker-compose.yml up
-  docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -v /dev/shb:/dev/shm -e DISPLAY=$DISPLAY --net host --device /dev/input --name rpg zivlazarov/rpg:1.1 \
+  docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -v /dev/shb:/dev/shm -e DISPLAY=$DISPLAY --net host \
+    --device /dev/input --device /dev/dri --name rpg zivlazarov/rpg:1.1 \
     bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/ && cd /workspace/build/bin && ./main"
 elif [ "$1" == "test" ]; then
   echo "Compiling and running tests..."
