de118fec87ee71e9d8cbe90898b00e0d1537f1ff
Ziv Lazarov <70108726+lazarovziv@users.noreply.github.com>
2024-08-09 11:53:49 +0300

    github actions & dockerfiles: created a windows image (#13)
    * github actions: added a windows build action
    
    * github actions: separated image build and compilation
    created a new action for linux compilation
    
    * dockerfiles: created a windows image
    
    * dockerfiles: added chocolatey to image
    
    * dockerfiles: added vulkan sdk installation
    didnt install runtime yet
    
    * github actions: removed custom runner image
    not supported yet so tests are running right after compilation finishes
    
    * cmake: using vulkan on unix based systems
    for the moment

--- linux-compile.sh
+++ linux-compile.sh
@@ -14,6 +14,6 @@ elif [ "$1" == "test" ]; then
 else
   echo "Compiling..."
   #  docker-compose -f dockerfiles/linux-compile-docker-compose.yml up
-  docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -v /dev/shb:/dev/shm -e DISPLAY=$DISPLAY --net host --name rpg zivlazarov/rpg:1.1 \
+  docker run --rm -v $(pwd):/workspace --name rpg zivlazarov/rpg:1.1 \
     bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/"
 fi
