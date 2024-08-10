f53a70f97ad15d276d9f22fa422a4e66f2656573
Ziv Lazarov <zivlazarov@gmail.com>
2024-08-10 22:10:29 +0300

    vulkan: added shaders compilation command before running vulkan app

--- linux-compile.sh
+++ linux-compile.sh
@@ -29,11 +29,13 @@ elif [ "$COMMAND" == "test" ]; then
 elif [ "$COMMAND" == "vulkan" ]; then
   if [ "$FIRST_OPTION" == "run" ]; then
     echo "Compiling and running Vulkan components..."
-    docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix -v /dev/shm:/dev/shm \
+    docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix \
       -e DISPLAY=$DISPLAY -w /workspace/MyRPGGame/vulkan --net host \
       --device /dev/input --device /dev/dri --user ubuntu --name rpg $DOCKER_IMAGE \
-      bash -c "cmake -D GLFW_BUILD_X11=1 -D GLFW_BUILD_WAYLAND=0 -B build/ -G \"Ninja\" . && \
-       cmake --build build/ $SECOND_OPTION && cd build/bin && ./vulkan"
+      bash -c "echo \"Compiling shaders...\" && glslc shaders/shader.vert -o shaders/vert.spv && \
+      glslc shaders/shader.frag -o shaders/frag.spv && echo \"Shaders compiled!\" && \
+      cmake -D GLFW_BUILD_X11=1 -D GLFW_BUILD_WAYLAND=0 -B build/ -G \"Ninja\" . && \
+      cmake --build build/ $SECOND_OPTION && cd build/bin && ./vulkan"
   else
     echo "Compiling Vulkan components..."
     #  docker-compose -f dockerfiles/linux-compile-docker-compose.yml up
