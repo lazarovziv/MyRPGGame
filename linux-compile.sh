eaa9097a8544d0fe2cb6f31b059fdf19840eddbb
Ziv Lazarov <zivlazarov@gmail.com>
2024-08-10 16:16:20 +0300

    automation: added conatiner user to be the same as host user

--- linux-compile.sh
+++ linux-compile.sh
@@ -15,14 +15,14 @@ if [ "$COMMAND" == "run" ]; then
 #  docker-compose -f dockerfiles/linux-start-docker-compose.yml up
   docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix \
     -e DISPLAY=$DISPLAY --net host \
-    --device /dev/input --device /dev/dri --name rpg $DOCKER_IMAGE \
+    --device /dev/input --device /dev/dri --user ubuntu --name rpg $DOCKER_IMAGE \
     bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/ && cd /workspace/build/bin && ./main"
 
 elif [ "$COMMAND" == "test" ]; then
   echo "Compiling and running tests..."
 #  docker-compose -f dockerfiles/linux-compile-docker-compose.yml up
   docker run --rm -v $(pwd):/workspace -v /tmp/.X11-unix:/tmp.X11-unix \
-    -e DISPLAY=$DISPLAY --net host --name rpg $DOCKER_IMAGE \
+    -e DISPLAY=$DISPLAY --net host --user ubuntu --name rpg $DOCKER_IMAGE \
     bash -c "cmake -B build/ -G \"Ninja\" . && cmake --build build/ $SECOND_OPTION && \
      cd /workspace/build/bin && ./tests"
 
